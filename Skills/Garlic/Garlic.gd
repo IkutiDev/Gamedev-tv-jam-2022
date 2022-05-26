523b9084dbd6cc09f8a9f23fb24f6faa01d5edeb
Ikuti <m.j.olkiewicz@gmail.com>
2022-05-26 23:37:55 +0200

    Added visuals + folder structure cleanup

--- Skills/Garlic/Garlic.gd
+++ Skills/Garlic/Garlic.gd
@@ -3,8 +3,8 @@ extends Node2D
 #Enemies
 
 var enemies : Array
-var garlicTimer = 0.5
-var damage = 10
+export var garlicSpeed = 0.5
+export var damage = 10
 
 var currentGarlicTimer = 0.0
 
@@ -14,7 +14,7 @@ func _process(delta):
 	if enemies.size() == 0:
 		return
 	currentGarlicTimer += delta
-	if currentGarlicTimer > garlicTimer:
+	if currentGarlicTimer > garlicSpeed:
 		currentGarlicTimer = 0.0
 		for i in enemies.size():
 			_dealDamage(enemies[i])
@@ -25,11 +25,8 @@ func _dealDamage(entity):
 
 func _on_Area2D_area_entered(area):
 	if area is Health:
-		_dealDamage(area)
 		enemies.append(area)
 
 
 func _on_Area2D_area_exited(area):
-	var enemyIndex = enemies.find(area)
-	if enemyIndex > -1:
-		enemies.remove(enemyIndex)
+	enemies.erase(area)
